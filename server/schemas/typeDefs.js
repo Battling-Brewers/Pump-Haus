ef217a4cf6a8fd17bb48a18f9095c89a387e9d55
daniel <yoojeenkim@gmail.com>
2021-11-22 17:28:12 -0500

    added user typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        user: User
+    }
+
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
+`;
+
+module.exports = typeDefs;
