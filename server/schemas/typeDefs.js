3d1e742472c8bfa99c16e6b269bd139e862960a8
Josh Watkins <joshwatkinssfs@gmail.com>
2021-11-23 12:13:08 -0500

    typeDef relations established

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,10 +2,32 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-        _id: ID
+        _id: ID!
         firstName: String
         lastName: String
         email: String
+        shipping: String
+    }
+
+    type Image {
+        url: String
+        products: [Product] @relation
+    }
+
+    type Product {
+        _id: ID!
+        prodName: String!
+        prodDescrip: String
+        price: Float!
+        prodImages: [Image] @relation
+        tags: [Tag] @relation
+    }
+
+    type Tag {
+        _id: ID!
+        tagName: String
+        tagDescription: String
+        products: [Product] @relation
     }
 
     type Auth {
@@ -15,10 +37,15 @@ const typeDefs = gql`
 
     type Query {
         user: User
+        products: [Product]
+        product(_id: String!): Product
+        tags(_id: String!): Tag
     }
 
     type Mutation {
         addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        delUser(_id: String!) : Auth
+        updateUser(id: String!) : Auth
         login(email: String!, password: String!): Auth
     }
 `;
