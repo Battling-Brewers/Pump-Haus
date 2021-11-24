7ea9c27f698c68ea8680d8a24b78ba6aff309a86
Josh Watkins <joshwatkinssfs@gmail.com>
2021-11-24 13:41:18 -0500

    typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,53 +1,80 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        firstName: String
-        lastName: String
-        email: String
-        shipping: String
-    }
+  type User {
+    _id: ID!
+    firstName: String
+    lastName: String
+    email: String!
+    password: String!
+    shippingAddy: String
+  }
 
-    type Image {
-        url: String
-        products: [Product] @relation
-    }
+  type Product {
+    _id: ID!
+    prodName: String!
+    prodDescrip: String
+    price: Float!
+    prodImage: [String]
+    tags: [Tag] @relation
+  }
 
-    type Product {
-        _id: ID!
-        prodName: String!
-        prodDescrip: String
-        price: Float!
-        prodImages: [Image] @relation
-        tags: [Tag] @relation
-    }
+  type Tag {
+    _id: ID!
+    tagName: String!
+    tagDescription: String
+  }
 
-    type Tag {
-        _id: ID!
-        tagName: String
-        tagDescription: String
-        products: [Product] @relation
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Auth {
-        token: ID
-        user: User
-    }
+  type Query {
+    user: User
+    products: [Product]
+    product(_id: String!): Product
+    tags: [Tag]
+    tag(_id: String!): Tag
+  }
 
-    type Query {
-        user: User
-        products: [Product]
-        product(_id: String!): Product
-        tags(_id: String!): Tag
-    }
-
-    type Mutation {
-        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-        delUser(_id: String!) : Auth
-        updateUser(id: String!) : Auth
-        login(email: String!, password: String!): Auth
-    }
+  type Mutation {
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+      shippingAddy: String
+    ): Auth
+    updateUser(
+      _id: String!
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+      shippingAddy: String
+    ): Auth
+    login(email: String!, password: String!): Auth
+    addProduct(
+      prodName: String!
+      prodDescrip: String
+      price: Float!
+      prodImage: [String]
+      tags: [String]
+    ): Product
+    updateProduct(
+      _id: String!
+      prodName: String!
+      prodDescrip: String
+      price: Float!
+      prodImage: [String!]
+      tags: [String]
+    ): Product
+    delProduct(_id: String!): Product
+    addTags(tagName: String!, tagDescription: String!): Tag
+    updateTags(_id: String!, tagName: String, tagDescription: String): Tag
+    delTags(_id: String!): Tag
+  }
 `;
 
 module.exports = typeDefs;
