1dbddb9f0c61eaafb9493339a5c98fbac8daa3a8
Josh Watkins <joshwatkinssfs@gmail.com>
2021-11-29 14:27:20 -0500

    images on product cards

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -3,10 +3,14 @@ import './productcard.css';
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 
 const ProductCard = ({ item }) => {
+    
+    let imgSrc = `/images/${item.prodImage[0]}`
+    console.log(imgSrc)
     return (
         <div className="card-container">
             <div className="circle"></div>
-            <img src={item.prodImage[0]} alt="product item"/>
+            <img className="img1" src={`/images/${item.prodImage[0]}`} alt="product item"/>
+            <img className="img2" src={`/images/${item.prodImage[1]}`} alt="product item"/>
             <div className="info">
                 {item.prodName}
                 {item.prodDescrip}
