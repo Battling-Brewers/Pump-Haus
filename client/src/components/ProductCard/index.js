a9cda5cb1f59e5fe800bfb3537cd5cd3984aec46
Josh Watkins <joshwatkinssfs@gmail.com>
2021-11-29 18:20:21 -0500

    prod cards styled for prod list

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,28 +1,43 @@
 import React from "react";
-import './productcard.css';
-import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+import "./productcard.css";
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
 
 const ProductCard = ({ item }) => {
-    
-    let imgSrc = `/images/${item.prodImage[0]}`
-    console.log(imgSrc)
-    return (
-        <div className="card-container">
-            <div className="circle"></div>
-            <img className="img1" src={`/images/${item.prodImage[0]}`} alt="product item"/>
-            <img className="img2" src={`/images/${item.prodImage[1]}`} alt="product item"/>
-            <div className="info">
-                {item.prodName}
-                {item.prodDescrip}
-                {item.price}
-                {item.quantity}
-                {item.tags[0].tagName}
-                <ShoppingCartOutlined className="icon"/>
-                <SearchOutlined className="icon"/>
-                <FavoriteBorderOutlined className="icon"/>
-            </div>
+  let priceFormat = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumFractionDigits: 2,
+  })
+  let price = priceFormat.format(item.price)
+  console.log(price)
+  return (
+    <div className="card-container">
+      <div className="circle"></div>
+      <img
+        className="img1"
+        src={`/images/${item.prodImage[0]}`}
+        alt="product item"
+      />
+      <img
+        className="img2"
+        src={`/images/${item.prodImage[1]}`}
+        alt="product item"
+      />
+      <div className="info">
+        <h4 className="prod-title">{item.prodName}</h4>
+        <h6 className="prod-title">{price}</h6>
+        <div className="iconz">
+          <ShoppingCartOutlined className="icon" />
+          <SearchOutlined className="icon" />
+          <FavoriteBorderOutlined className="icon" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProductCard;
